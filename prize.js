119487f6634998a7ae4b9110cc5c031643d5b374
febriano <rinoakbr@gmail.com>
2022-04-01 17:51:13 +0700

    bismillah cepet dapet prize aamiin

--- prize.js
+++ prize.js
@@ -6,15 +6,55 @@ require('dotenv').config();
 // client.basePath = "https://some-other-host"
 // Configure Gate APIv4 key authentication:
 client.setApiKeySecret(process.env.key, process.env.secret);
+const axios = require('axios');
+const crypto = require('crypto');
+const qs = require('qs');
 
+const binanceConfig = {
+  API_KEY: process.env.toko_key,
+  API_SECRET: process.env.toko_secret,
+  HOST_URL: 'https://www.tokocrypto.com',
+};
+
+const buildSign = (data, config) => {
+  return crypto.createHmac('sha256', config.API_SECRET).update(data).digest('hex');
+};
+
+const privateRequest = async (data, endPoint, type) => {
+  const dataQueryString = qs.stringify(data);
+  const signature = buildSign(dataQueryString, binanceConfig);
+  const requestConfig = {
+    method: type,
+    url: binanceConfig.HOST_URL + endPoint + '?' + dataQueryString + '&signature=' + signature,
+    headers: {
+      'X-MBX-APIKEY': `${binanceConfig.API_KEY}`,
+    },
+  };
+
+  try {
+    console.log('URL: ', requestConfig.url);
+    const response = await axios(requestConfig);
+    console.log(response.data);
+    return response;
+  }
+  catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+const data = {
+    asset: 'NEAR',
+  //   recvWindow: 20000,
+    timestamp: Date.now(),
+  };
 const api = new GateApi.WalletApi(client);
 const opts = {
   'currency': 'USD' // string | Currency unit used to calculate the balance amount. BTC, CNY, USD and USDT are allowed. USDT is the default.
 };
 api.getTotalBalance(opts)
-   .then(value => parseInt(value.body.total.amount)!==0?sendMessage("<b>PRIZE IS RELEASED!!!!</b><a href='https://akash.bigdipper.live/account/akash1xw6qvxd4lsmm8kwz8lv0gfsrzemlwnz9zcjly3'>akash1xw6qvxd4lsmm8kwz8lv0gfsrzemlwnz9zcjly3</a>"):sendMessage2("still no prize"),
+   .then(value => parseInt(value.body.total.amount)!==0?sendMessage("<b>PRIZE IS RELEASED!!!!</b><a href='https://akash.bigdipper.live/account/akash1xw6qvxd4lsmm8kwz8lv0gfsrzemlwnz9zcjly3'>akash1xw6qvxd4lsmm8kwz8lv0gfsrzemlwnz9zcjly3</a>"):sendMessage2("still no prize from akash team"),
          error => sendMessage2(JSON.stringify(error)));
-
+         privateRequest(data, '/open/v1/account/spot/asset', 'GET').then(a=>parseInt(a.data.data.free)===0&&parseInt(a.data.data.locked)===0?sendMessage2("still no prize from near team"):sendMessage("<b>PRIZE FROM NEAR IS RELEASED YEAH!!!</b>")).catch(e=>sendMessage2("error near"));
          async function sendMessage2(message){
     
 
@@ -39,4 +79,6 @@ api.getTotalBalance(opts)
             bot.sendMessage(process.env.bot, message,{parse_mode:"HTML"});
             // return bot.stopPolling() 
         }
-        
+
+    
+  
